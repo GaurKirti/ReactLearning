1e12172af378af419234543cc32cd5b3518cd9b2
GaurKirti <kirtigaur11@gmail.com>
2023-12-10 11:01:50 +0530

    Initial changes

---
+++ App.js
@@ -0,0 +1,3 @@
+const headingForReact = React.createElement("h1",{id:"heading", xyz:"abc"},"hello world from React");
+            const root = ReactDOM.createRoot(document.getElementById("root1"));
+            root.render(headingForReact);
