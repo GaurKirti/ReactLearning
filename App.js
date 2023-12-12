8d56ca3af1f0bac194dd88ed83f5e2588b6c9c93
GaurKirti <kirtigaur11@gmail.com>
2023-12-12 09:08:43 +0530

    delta

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const headingForReact = React.createElement("h1",{id:"heading", xyz:"abc"},"hello world from React");
             const root = ReactDOM.createRoot(document.getElementById("root1"));
             root.render(headingForReact);
